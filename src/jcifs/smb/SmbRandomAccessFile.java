7fe7cbd65b866458b0a92d8f2769a8886580d88b
dmitriy <jaddressbook@gmail.com>
2023-08-02 17:38:59 -0700

    fixed bug with exceeding adding pointer position

--- src/jcifs/smb/SmbRandomAccessFile.java
+++ src/jcifs/smb/SmbRandomAccessFile.java
@@ -105,7 +105,7 @@ public class SmbRandomAccessFile implements DataOutput, DataInput {
             count = this.read( b, off + n, len - n );
             if( count < 0 ) throw new SmbException( "EOF" );
             n += count;
-            fp += count;
+            //fp += count;
         } while( n < len );
     }
     public int skipBytes( int n ) throws SmbException {
